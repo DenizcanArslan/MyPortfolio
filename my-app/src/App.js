aad27eff435ad48d5dc9a480e79b0f7881468777
Denizcan <denizcan.arslan@outlook.com>
2023-12-21 21:37:26 +0200

    navbar component was added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-        
-    </div>
+   <>
+     <Navbar/>
+     
+   </>
+       
+ 
   );
 }
 
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+
+import logo from "../assets/images/denizcanArslanLogo.jpeg"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+    
+     const [isMobile, setIsMobile] = useState(false);
+
+     const handleResize = () => {
+       setIsMobile(window.innerWidth <= 991);
+     };
+
+    
+
+  
+     useEffect(() => {
+       // Add event listener for window resize
+      window.addEventListener('resize', handleResize);
+      
+    //   // Initial check for mobile view
+       handleResize();
+  
+    //   // Remove event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', handleResize);
+     };
+     }, []);
+
+    
+
+  return (
+      <>
+    <nav className="navbar navbar-expand-lg bg-white text-capitalize ">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={logo} alt="Denizcan Arslan Logo" className="img-fluid"  style={{ width: '10rem' }} />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mx-2">
+            <li className="nav-item px-3">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+           
+            <li className="nav-item  px-3">
+              <a className="nav-link" href="#">
+                Tech Stack
+              </a>
+            </li>
+            <li className="nav-item  px-3">
+              <a className="nav-link" href="#">
+                Portfolio
+              </a>
+            </li>
+            <li className={`nav-item px-3 ${isMobile ? '' : ' border-end'}`}>
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+
+            <li className={`nav-item  px-1 `}>
+              <a className="nav-link" href="https://github.com/DenizcanArslan" target="_blank">
+              <i className="fa-brands fa-github"></i>
+              </a>
+            </li>
+            <li className={`nav-item  px-1 `}>
+              <a className="nav-link" href="https://www.instagram.com/denizcan.arslan/" target="_blank">
+              <i className="fa-brands fa-instagram"></i>             
+               </a>
+            </li>
+         
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+</>
+   
+
+  );
+  
+};
+
+
+
+export default Navbar;
